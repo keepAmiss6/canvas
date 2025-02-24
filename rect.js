601efbd7c1e40b8090ddb3f96345caf1316cc0be
zhangyan <zhangy-ej@glodon.com>
2025-02-24 18:23:17 +0800

    feat：抽取上层方法&重绘

--- rect.js
+++ rect.js
@@ -1,36 +1,42 @@
 var dom = document.getElementById('rectDrawing');
 var context = dom.getContext('2d');
+function clearCanvas() {
+    context.clearRect(0, 0, dom.width, dom.height);
+}
 var Rect = /** @class */ (function () {
-    function Rect(x, y, width, height) {
+    function Rect(x, y, width, height, color) {
+        if (color === void 0) { color = 'green'; }
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.color = color;
         this.draw();
     }
     Rect.prototype.draw = function () {
-        context.clearRect(0, 0, 1000, 1000); //清除整个画布
-        context.fillStyle = 'red';
+        // context.clearRect(0, 0, 1000, 1000) //清除整个画布
+        context.fillStyle = this.color;
         context.fillRect(this.x, this.y, this.width, this.height);
-        // const rectDom = document.getElementById('rectDrawing') as HTMLCanvasElement
-        // if (rectDom && rectDom.getContext) {
-        //     const context = rectDom.getContext('2d')
-        //     context.clearRect(0,0,1000,1000) //清除整个画布
-        //     context.fillStyle = 'red'
-        //     context.fillRect(this.x, this.y, this.width, this.height)
-        // }
     };
     Rect.prototype.setAttr = function (name, value) {
         this[name] = value;
-        this.draw();
+        this.reDraw();
+    };
+    Rect.prototype.reDraw = function () {
+        clearCanvas();
+        rects.forEach(function (rect) {
+            rect.draw();
+        });
     };
     return Rect;
 }());
-var rect1 = new Rect(10, 10, 100, 100);
+var rects = [];
+var rect1 = new Rect(10, 10, 100, 100, 'yellow');
+var rect2 = new Rect(100, 100, 20, 300, 'red');
+rects.push(rect1, rect2);
 setTimeout(function () {
     rect1.setAttr('width', 1000);
 }, 2000);
-// const rect2 = new Rect(90, 50, 150, 50)
-// setTimeout(() => {
-//     rect2.setAttr('width', 1000)
-// }, 2000)
+setTimeout(function () {
+    rect2.setAttr('width', 100);
+});
--- rect.ts
+++ rect.ts
@@ -1,44 +1,55 @@
 const dom = document.getElementById('rectDrawing') as HTMLCanvasElement
 const context = dom.getContext('2d')
+
+function clearCanvas() {
+    context.clearRect(0, 0, dom.width, dom.height)
+}
+
 class Rect {
     private x: number;
     private y: number;
     private width: number;
     private height: number;
+    private color: string;
 
-    constructor(x: number, y: number, width: number, height: number) {
+    constructor(x: number, y: number, width: number, height: number, color: string = 'green') {
         this.x = x
         this.y = y
         this.width = width
         this.height = height
+        this.color = color
         this.draw()
     }
 
     draw(): void {
-        context.clearRect(0,0,1000,1000) //清除整个画布
-        context.fillStyle = 'red'
+        // context.clearRect(0, 0, 1000, 1000) //清除整个画布
+        context.fillStyle = this.color
         context.fillRect(this.x, this.y, this.width, this.height)
-        // const rectDom = document.getElementById('rectDrawing') as HTMLCanvasElement
-        // if (rectDom && rectDom.getContext) {
-        //     const context = rectDom.getContext('2d')
-        //     context.clearRect(0,0,1000,1000) //清除整个画布
-        //     context.fillStyle = 'red'
-        //     context.fillRect(this.x, this.y, this.width, this.height)
-        // }
     }
 
     setAttr(name: string, value: number): void {
         this[name] = value
-        this.draw()
+        this.reDraw()
+    }
+
+    reDraw(): void {
+        clearCanvas()
+        rects.forEach(rect => {
+            rect.draw()
+        })
     }
 
 }
 
-const rect1 = new Rect(10, 10, 100, 100)
-setTimeout(()=>{
-    rect1.setAttr('width',1000)
-},2000)
-// const rect2 = new Rect(90, 50, 150, 50)
-// setTimeout(() => {
-//     rect2.setAttr('width', 1000)
-// }, 2000)
+const rects: Rect[] = []
+const rect1 = new Rect(10, 10, 100, 100, 'yellow')
+const rect2 = new Rect(100, 100, 20, 300, 'red')
+rects.push(rect1, rect2)
+
+setTimeout(() => {
+    rect1.setAttr('width', 1000)
+}, 2000)
+setTimeout(() => {
+    rect2.setAttr('width', 100)
+})
+

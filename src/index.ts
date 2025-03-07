e023a3e0fd34936c02e181fa2a4a0b1b46f08a09
zhangyan <zhangy-ej@glodon.com>
2025-03-07 17:50:46 +0800

    feat：调整测试数据

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Rect } from './rect.js'
 import { Stage } from "./stage.js";
 
 const stage = new Stage()
-const rect1 = new Rect(10, 10, 100, 100, 'yellow')
+const rect1 = new Rect(440, 300, 100, 100, 'yellow')
 stage.appendChild(rect1)
 const rect2 = new Rect(500, 500, 20, 300, 'red')
 stage.appendChild(rect2)
@@ -18,6 +18,6 @@ setTimeout(() => {
   // rect1.setAttr('height', 200)
   rect2.setAttr('width', 400);
   // rect1.setAttr('width', 300)
-  rect1.setAttr('y', 600);
+  rect1.setAttr('width', 900);
 }, 3000)
 
